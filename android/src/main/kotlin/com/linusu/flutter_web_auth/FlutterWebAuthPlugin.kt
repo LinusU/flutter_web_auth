da0207d9ca921b4d0825611069db48bdef19ad15
Jun Ying <jying@simplec.com>
2021-03-26 19:56:28 -0400

    Added Plugin API V2.

--- android/src/main/kotlin/com/linusu/flutter_web_auth/FlutterWebAuthPlugin.kt
+++ android/src/main/kotlin/com/linusu/flutter_web_auth/FlutterWebAuthPlugin.kt
@@ -12,14 +12,26 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class FlutterWebAuthPlugin(private val context: Context): MethodCallHandler {
+class FlutterWebAuthPlugin(private val context: Context): MethodCallHandler, FlutterPlugin {
   companion object {
     val callbacks = mutableMapOf<String, Result>()
 
+    @override
+    fun onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    }
+
+    @override
+    fun onDetachedFromFlutterEngine(binding: FlutterPluginBinding?) {
+    }
+
     @JvmStatic
     fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "flutter_web_auth")
-      channel.setMethodCallHandler(FlutterWebAuthPlugin(registrar.activity() ?: registrar.context()))
+        init(registrar)
+    }
+
+    fun init(registrar: Registrar) {
+        val channel = MethodChannel(registrar.messenger(), "flutter_web_auth")
+        channel.setMethodCallHandler(FlutterWebAuthPlugin(registrar.activity() ?: registrar.context()))
     }
   }
 
