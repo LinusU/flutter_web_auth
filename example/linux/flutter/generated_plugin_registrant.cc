4c997b544667ced22edbc9e415627b810f0a971c
Jonas Franz <info@jonasfranz.software>
2021-05-04 12:56:38 +0200

    Add initial linux support

---
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,13 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_web_auth/flutter_web_auth_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) flutter_web_auth_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterWebAuthPlugin");
+  flutter_web_auth_plugin_register_with_registrar(flutter_web_auth_registrar);
+}
