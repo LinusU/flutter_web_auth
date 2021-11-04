102ec04e9bd25551c3a8a3364c9da95207a3bd3c
Pavlo Bilohaienko <pavlo.bilohaienko@itomy.ch>
2021-11-04 11:34:47 +0200

    KAA-747 Extending delegate with plugin's protocol

--- ios/Classes/SwiftFlutterWebAuthPlugin.swift
+++ ios/Classes/SwiftFlutterWebAuthPlugin.swift
@@ -3,6 +3,10 @@ import SafariServices
 import Flutter
 import UIKit
 
+public protocol FlutterPresentationContextProviding: UIApplicationDelegate {
+    var flutterController: FlutterViewController { get }
+}
+
 public class SwiftFlutterWebAuthPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "flutter_web_auth", binaryMessenger: registrar.messenger())
@@ -46,11 +50,11 @@ public class SwiftFlutterWebAuthPlugin: NSObject, FlutterPlugin {
                 let session = ASWebAuthenticationSession(url: url, callbackURLScheme: callbackURLScheme, completionHandler: completionHandler)
 
                 if #available(iOS 13, *) {
-                    guard let provider = UIApplication.shared.visibleViewController(FlutterViewController.self) else {
+                    guard let appDelegate = UIApplication.shared.delegate as? FlutterPresentationContextProviding else {
                         result(FlutterError(code: "FAILED", message: "Failed to aquire root FlutterViewController" , details: nil))
                         return
                     }
-
+                    let provider = appDelegate.flutterController
                     session.prefersEphemeralWebBrowserSession = preferEphemeral
                     session.presentationContextProvider = provider
                 }
@@ -74,30 +78,5 @@ public class SwiftFlutterWebAuthPlugin: NSObject, FlutterPlugin {
 extension FlutterViewController: ASWebAuthenticationPresentationContextProviding {
     public func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
         return self.view.window!
-    }
-}
-
-extension UIApplication {
-    
-    /// Getting the VC in hierarchy by type.
-    /// Supporting navigation, tab bar and presented controllers.
-    func visibleViewController<Target: UIViewController>(_ target: Target.Type) -> Target? {
-        let root = UIApplication.shared.windows.first?.rootViewController
-        var visibleController: UIViewController?
-        if let navigationRoot = root as? UINavigationController {
-            visibleController = navigationRoot.topViewController
-        } else if let tabRoot = root as? UITabBarController {
-            visibleController = tabRoot.selectedViewController
-        } else {
-            // Common UIViewController
-            visibleController = root
-        }
-        if let modalController = visibleController?.presentedViewController as? Target {
-            return modalController
-        } else {
-            // If target not found as modal and the type cast below fails,
-            // the target controller is not presented in hierarchy.
-            return visibleController as? Target
-        }
     }
 }
