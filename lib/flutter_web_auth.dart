b85f3d3ac9485d95c79816b77935420d08f7682c
Humza Tareen <77065466+humzakt@users.noreply.github.com>
2022-07-26 17:12:05 +0500

    Removed null aware operators from not null widgets
    WidgetsBinding is never null

--- lib/flutter_web_auth.dart
+++ lib/flutter_web_auth.dart
@@ -30,8 +30,8 @@ class FlutterWebAuth {
   /// [callbackUrlScheme] should be a string specifying the scheme of the url that the page will redirect to upon successful authentication.
   /// [preferEphemeral] if this is specified as `true`, an ephemeral web browser session will be used where possible (`FLAG_ACTIVITY_NO_HISTORY` on Android, `prefersEphemeralWebBrowserSession` on iOS/macOS)
   static Future<String> authenticate({required String url, required String callbackUrlScheme, bool? preferEphemeral}) async {
-    WidgetsBinding.instance?.removeObserver(_resumedObserver); // safety measure so we never add this observer twice
-    WidgetsBinding.instance?.addObserver(_resumedObserver);
+    WidgetsBinding.instance.removeObserver(_resumedObserver); // safety measure so we never add this observer twice
+    WidgetsBinding.instance.addObserver(_resumedObserver);
     return await _channel.invokeMethod('authenticate', <String, dynamic>{
       'url': url,
       'callbackUrlScheme': callbackUrlScheme,
@@ -44,6 +44,6 @@ class FlutterWebAuth {
   /// terminate all `authenticate` calls with an error.
   static Future<void> _cleanUpDanglingCalls() async {
     await _channel.invokeMethod('cleanUpDanglingCalls');
-    WidgetsBinding.instance?.removeObserver(_resumedObserver);
+    WidgetsBinding.instance.removeObserver(_resumedObserver);
   }
 }
