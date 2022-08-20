4651ccd6d57fa415f33cdf4bfdb0f285fc506301
Nico Mexis <nico.mexis@kabelmail.de>
2022-08-20 16:41:39 +0200

    Add more complete examples

---
+++ flutter_web_auth_2_platform_interface/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_web_auth_2
+import url_launcher_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
---
+++ flutter_web_auth_2_windows/example/android/app/src/main/kotlin/com/linusu/flutter_web_auth_2_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.linusu.flutter_web_auth_2_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
